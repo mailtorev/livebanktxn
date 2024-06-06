ee1c02e9b0b3ef50d8cb8e97236bc31ddc604837
mailtorev <58803319+mailtorev@users.noreply.github.com>
2024-06-06 15:23:13 -0400

    Initial Code from Spring IO Initializer

---
+++ src/main/java/com/live/trn/TrnApplication.java
@@ -0,0 +1,13 @@
+package com.live.trn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/live/trn/TrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.trn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
